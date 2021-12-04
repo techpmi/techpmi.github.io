5cae5c1058268695436e57f93fc5fc521c2b2421
Ilya Povarov <cook0208@mail.ru>
2021-12-04 19:43:31 +0300

    добавлена возможность поиска по всем колонкам в таблице

--- src/Pages/StudentRegistry.tsx
+++ src/Pages/StudentRegistry.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'react';
 import Page from '../Components/Page';
-import RegistryTable from '../Components/RegistryTable';
+import RegistryTable, { Column } from '../Components/RegistryTable';
 import { studentTableColumns, students } from '../mockdata';
 
 // подумать, нужен ли нам этот интерфейс
@@ -10,16 +10,56 @@ interface StudentRegistryData {
     degree: string;
 }
 
+interface StudentRegistryPageState {
+    pattern: string;
+}
+
 // TODO: Сверстать реестр студентов
 
-class StudentRegistry extends Component {
-    render() { 
+class StudentRegistry extends Component<{}, StudentRegistryPageState> {
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            pattern: '',
+        };
+
+        this.handlePressEnter = this.handlePressEnter.bind(this);
+    }
+
+    handlePressEnter(event: any): void {
+        if (event.key === 'Enter') {
+            this.setState({
+                pattern: event.target.value.toLowerCase(),
+            });
+        }
+    }
+
+    filterData(data: Record<string, string>[]): Record<string, string>[] {
+        return data.filter((row: Record<string, string>) => {
+            let isAnyFieldMatchesPattern: boolean = false;
+
+            studentTableColumns.forEach((column: Column) => {
+                const field = row[column.field];
+                if (field.toLowerCase().indexOf(this.state.pattern) !== -1) {
+                    isAnyFieldMatchesPattern = true;
+                }
+            });
+            return isAnyFieldMatchesPattern;
+        });
+    }
+
+    render() {
         return (
             <Page type="registry" caption="Реестр студентов">
-                <RegistryTable columns={studentTableColumns} data={students} />
+                <>
+                    <div className="Registry-input-wrapper">
+                        <input type="search" className="Registry-input" placeholder="Поиск" onKeyDown={this.handlePressEnter} />
+                    </div>
+                    <RegistryTable columns={studentTableColumns} data={this.filterData(students)} />
+                </>
             </Page>
         );
     }
 }
- 
+
 export default StudentRegistry;
